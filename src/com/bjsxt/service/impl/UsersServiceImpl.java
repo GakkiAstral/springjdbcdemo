d4aedece494f77cf62462e4c75592e3777e20df4
tongliang <852481695@qq.com>
2021-05-13 20:10:34 +0800

    Initial commit

---
+++ src/com/bjsxt/service/impl/UsersServiceImpl.java
@@ -0,0 +1,49 @@
+package com.bjsxt.service.impl;
+
+import com.bjsxt.dao.UsersDao;
+import com.bjsxt.pojo.Users;
+import com.bjsxt.service.UsersService;
+
+import java.util.List;
+
+public class UsersServiceImpl implements UsersService {
+    private UsersDao usersDao;
+
+    public UsersDao getUsersDao() {
+        return usersDao;
+    }
+
+    public void setUsersDao(UsersDao usersDao) {
+        this.usersDao = usersDao;
+    }
+
+    /**
+     * 添加用户
+     * @param users
+     * @return
+     */
+    @Override
+    public int addUsers(Users users) {
+        return this.usersDao.insertUsers(users);
+    }
+
+    /**
+     * 批量添加用户
+     * @param users
+     * @return
+     */
+    @Override
+    public int[] addUsers(List<Users> users) {
+        return this.usersDao.batchInsertUsers(users);
+    }
+
+    /**
+     * 根据id查询用户
+     * @param userid
+     * @return
+     */
+    @Override
+    public Users findUsersById(int userid) {
+        return this.usersDao.selectUsersById(userid);
+    }
+}
---
+++ src/com/bjsxt/test/AddUsersTest.java
@@ -0,0 +1,18 @@
+package com.bjsxt.test;
+
+import com.bjsxt.pojo.Users;
+import com.bjsxt.service.UsersService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AddUsersTest {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UsersService usersService = (UsersService) applicationContext.getBean("usersService");
+        Users users = new Users();
+        users.setUsername("suibian");
+        users.setUsersex("male");
+        int i = usersService.addUsers(users);
+        System.out.println(i);
+    }
+}
